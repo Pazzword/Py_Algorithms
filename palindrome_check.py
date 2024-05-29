91e4932cf1103eebcaf2c7004c49ce5b0f691016
Anzor <anzor@Anzors-iMac.local>
2024-05-29 05:33:32 +0100

    update

--- palindrome_check.py
+++ palindrome_check.py
@@ -1,6 +1,16 @@
+# For words
 def is_palindrome(s):
     return s == s[::-1]
 
 example = "Anzor"
 
 print(is_palindrome(example))
+
+
+# For numbers
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        s = str(x)
+        return s == s[::-1]
