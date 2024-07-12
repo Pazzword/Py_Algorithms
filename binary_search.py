f0542e426c2e21606473c867310dc00eed5ce7ef
Anzor <anzor.pozner@gmail.com>
2024-07-12 07:50:33 +0100

    update

--- binary_search.py
+++ binary_search.py
@@ -1,15 +1,17 @@
-def binary_search(arr, target):
-    left, right = 0, len(arr) - 1
-    while left <= right:
-        mid = (left+right) // 2
-        if arr[mid] == target:
-            return mid
-        elif arr[mid] < target:
-            left = mid + 1
-        else:
-            right = mid - 1
-    return -1
-    
+from typing import List
+
+class Solution:
+    def binary_search(self, arr: List[int], x: int) -> int:
+        left, right = 0, len(arr) + 1
+        while left <= right:
+            mid = (left + right)  // 2
+            if arr[mid] == x:
+                return mid
+            elif arr[mid] < x:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return - 1
 
 example_arr = [1,23,13,42,51,26,7,8,19,10,11]
 
@@ -18,4 +20,4 @@ example_target = 42
 sorted_arr = sorted(example_arr)
 
 print(sorted_arr)
-print(binary_search(sorted_arr, example_target))
+print(Solution().binary_search(sorted_arr, example_target))
